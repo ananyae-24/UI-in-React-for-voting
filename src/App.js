27efc46e8765d2fa98cb19ae412aadad4aa5ab57
ananyae-24 <ananyae123@gmail.com>
2020-08-14 12:13:55 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import style from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={style.App}>
+      <header className={style.App_header}>
+        <img src={logo} className={style.App_logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
